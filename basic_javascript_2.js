b46a8dee3947e952661989c31440a153e1002f2a
ChristopherCordsen <c-cordsen@hotmail.com>
2019-09-05 12:48:38 -0700

    Committ to GitHub
    Commiting per gare 62, I need help with
    basic_javascript_2.js and operator.js

---
+++ basic_javascript_2.js
@@ -0,0 +1,11 @@
+function ClickingFunctions() {
+    var String = "Blue Feathers are Magical!";
+    var result = String.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function myFunction()   {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
---
+++ js/basic_javascript_2.2.js
@@ -0,0 +1,5 @@
+function ClickingFunctions() {
+    var String = "Blue Feathers are Magical!";
+    var result = String.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
