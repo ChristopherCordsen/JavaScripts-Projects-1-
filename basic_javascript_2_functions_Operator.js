bfc15d1e00aea68d9e0563159e27004611c4b4ac
ChristopherCordsen <c-cordsen@hotmail.com>
2019-09-05 17:42:43 -0700

    Adding in Operator and Functions

--- basic_javascript_2_functions_Operator.js
+++ basic_javascript_2_functions_Operator.js
@@ -6,6 +6,6 @@ function ClickingFunctions() {
 
 function myFunction()   {
     var sentence = "I am learning";
-    sentence += "a lot from this book!";
-    document.getElementById("concatenate").innerHTML = sentence;
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
